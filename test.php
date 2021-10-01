f7d96f9ca4e4c594239ce692401856666bcd12e8
newsplash24 <mohamed.hamed201328@gmail.com>
2021-10-01 23:20:31 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,17 @@
 <?php
- header("Location: MyApp://somestring;")(
+ header("Location: cashminute://somestring;")(
 ?>
 <html>
  <head>
-  <meta http-equiv="Refresh" content="0; MyApp://somestring" />
+  <meta http-equiv="Refresh" content="0; cashminute://somestring" />
   <title>Opening App...</title>
   <script>
    function openApp() {
-    window.location.href = "MyApp://somestring";
+    window.location.href = "cashminute://somestring";
    }
   </script>
  </head>
  <body onload="openApp();">
-  <a href="MyApp://somestring">Click here if app doesn't open...</a>
+  <a href="cashminute://somestring">Click here if app doesn't open...</a>
  </body>
 </html>
